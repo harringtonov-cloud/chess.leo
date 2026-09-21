328eb174dd6c4626d1d9320c6f1d9fd0e623b764
harringtonov-cloud <harringtonov@gmail.com>
2026-09-21 14:05:07 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,16 +7,7 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        'bg-dark': '#161512',
-        'bg-secondary': '#262421',
-        'bg-tertiary': '#2d2b28',
-        'board-light': '#f0d9b5',
-        'board-dark': '#b58863',
-        'accent': '#759900',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
